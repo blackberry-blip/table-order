e0774062224d551b765f6ababa439c7ce24d30be
blackberry-blip <bhurk7@gmail.com>
2026-08-02 16:43:32 +0530

    The table page stays completely clean for customers. Staff get the installable app experience.

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "table-order-staff-v1";
+const urlsToCache = [
+  "/",
+  "/reception",
+  "/kitchen",
+  "/icon-192x192.png",
+  "/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
